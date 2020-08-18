3cce8512fb2e49e8be1341deb49597752b339cde
zjnmarsh <zjnmarsh@gmail.com>
2020-08-18 12:57:20 +0100

    ECA now works with multiple generations

--- elementary_cellular_automata.py
+++ elementary_cellular_automata.py
@@ -9,22 +9,28 @@ class CA:
         self.cells = [0] * width
         self.new_cells = [None] * width
         self.cells[int((width / 2) - 1)] = 1
+        self.generations = generations
 
     def rules(self, a, b, c, i):
         index = int((a + b + c), 2)
+        # print(a,b,c)
         ruleset = [0, 1, 0, 1, 1, 0, 1, 0]
+        # print(index, ruleset[index])
         self.new_cells[i] = ruleset[index]
 
     def ca(self):
-        for i in range(self.width):
-            a = str(self.cells[((i - 1) % self.width)])
-            b = str(self.cells[i])
-            c = str(self.cells[((i + 1) % self.width)])
-            print(a,b,c)
-            self.rules(a, b, c, i)
         print(self.cells)
-        print(self.new_cells)
+        for gen in range(self.generations):
+            for i in range(self.width):
+                a = str(self.cells[((i - 1) % self.width)])
+                b = str(self.cells[i])
+                c = str(self.cells[((i + 1) % self.width)])
+                # print(a,b,c)
+                self.rules(a, b, c, i)
+            self.cells = self.new_cells
+            self.new_cells = [None]*self.width
+            print(self.cells)
 
 
-ca = CA(10)
+ca = CA(100,100)
 ca.ca()
