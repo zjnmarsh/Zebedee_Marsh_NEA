7b3639d2866371d628e63c299590b513df697aed
zjnmarsh <zjnmarsh@gmail.com>
2020-08-18 13:08:41 +0100

    Prints each generation in a clearer way

--- elementary_cellular_automata.py
+++ elementary_cellular_automata.py
@@ -2,6 +2,9 @@
 
 # grid = [0,1,0,0,1,1,0,1,1,0]
 
+import time
+
+
 class CA:
     def __init__(self, width, generations):
         self.ruleset = [0, 1, 1, 1, 1, 0, 1, 1]
@@ -10,6 +13,7 @@ class CA:
         self.new_cells = [None] * width
         self.cells[int((width / 2) - 1)] = 1
         self.generations = generations
+        self.cell_timeline = []
 
     def rules(self, a, b, c, i):
         index = int((a + b + c), 2)
@@ -18,8 +22,20 @@ class CA:
         # print(index, ruleset[index])
         self.new_cells[i] = ruleset[index]
 
+    def draw(self):
+        output = ""
+        for cell_state in self.cell_timeline:
+            for element in cell_state:
+                if element == 0:
+                    output += " " + " "
+                else:
+                    output += "X" + " "
+            print(output)
+            time.sleep(0.1)
+            output = ""
+
     def ca(self):
-        print(self.cells)
+        # print(self.cells)
         for gen in range(self.generations):
             for i in range(self.width):
                 a = str(self.cells[((i - 1) % self.width)])
@@ -28,9 +44,11 @@ class CA:
                 # print(a,b,c)
                 self.rules(a, b, c, i)
             self.cells = self.new_cells
-            self.new_cells = [None]*self.width
-            print(self.cells)
+            self.cell_timeline.append(self.cells)
+            self.new_cells = [None] * self.width
+            # print(self.cells)
+        self.draw()
 
 
-ca = CA(100,100)
+ca = CA(100, 100)
 ca.ca()
