ab50a44ebd8cefd162dc0aa68345b98a3a33bb44
zjnmarsh <zjnmarsh@gmail.com>
2020-08-27 18:16:10 +0100

    Added second test page with buttons allowing navigation between them

--- prototype_tkinter_first.py
+++ prototype_tkinter_first.py
@@ -15,26 +15,44 @@ class Gui(tk.Tk):
 
         self.frames = {}  # allows application to open different types of pages easily
 
-        frame = StartPage(container, self)
+        for F in (StartPage, PageOne):  # all pages need to be listed here
 
-        self.frames[StartPage] = frame
+            print(F)
 
-        frame.grid(row=0, column=0, sticky="nsew")
+            frame = F(container, self)
 
-        self.show_frame(StartPage)
+            self.frames[F] = frame  # saving classes to dictionary, "loading it in"
+
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame(StartPage)  # start page is shown first
 
     def show_frame(self, cont):
-        frame = self.frames[cont] # looks for cont in dict
+        frame = self.frames[cont]  # looks for cont in dict
         frame.tkraise()  # which is then raised
 
 
 class StartPage(tk.Frame):
 
-    def __init__(self, parent, controller): # parent class is Gui
-        tk.Frame.__init__(self, parent,)
+    def __init__(self, parent,
+                 controller):  # parent class is Gui, controller class is main class, allowing show_frame to be called
+        tk.Frame.__init__(self, parent)
 
         label = tk.Label(self, text="Start Page")
         label.pack(pady=10, padx=10)
+        button1 = tk.Button(self, text="Page 1", command=lambda: controller.show_frame(PageOne))
+        button1.pack()
+
+
+class PageOne(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+
+        label = tk.Label(self, text="This is page 1")
+        label.pack(pady=10, padx=10)
+        button2 = tk.Button(self, text="Start Page", command=lambda: controller.show_frame(StartPage))
+        button2.pack()
 
 
 app = Gui()
