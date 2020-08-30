95b860d48d3de5ddfb70adc993ea84cbabc404e4
zjnmarsh <zjnmarsh@gmail.com>
2020-08-30 13:02:32 +0100

    Two pages now work

--- prototype_tkinter_second.py
+++ prototype_tkinter_second.py
@@ -25,35 +25,38 @@ class Main_Window:
     def openSIR(self):
         # self.new_window = tk.Toplevel(self.master)
         # self.app = SIR_Window(self.new_window)
-        self.new_window = tk.Toplevel(self.master)
-        self.app = SIR_Window(self.new_window)
+        # self.new_window = tk.Toplevel(self.master)
+        # self.app = SIR_Window(self.new_window)
 
+        root2 = tk.Tk()
+        root2.title('Main Window')
+        root2.geometry('600x400')
+
+        new_window = SIR_Window(root2)
+        root2.mainloop()
 
 class SIR_Window:
 
     def __init__(self, master):
         self.master = master
-        # self.frame = tk.Frame(master)
-        master.title("SIR Simulation")
-        master.geometry('1440x900')
+        self.frame = ttk.Frame(master, padding=5)
 
-        self.lbl_name2 = ttk.Label(self.master, text='This is the SIR Page')
-        self.btn_ph = ttk.Button(self.master, text='Close', command=self.close_window)
+        self.lbl_name = ttk.Label(self.frame, text='This is the SIR model')
+        self.btn_SIR = ttk.Button(self.frame, text='Close', command=self.close)
 
-        self.lbl_name2.grid(column=1, row=0)
-        self.btn_ph.grid(column=1, row=1)
-        # self.lbl_name2.pack()
-        # self.btn_ph.pack()
+        self.frame.grid(row=0, column=0, sticky='nsew')
 
-        #
-        # self.master.columnconfigure(0, weight=1)
-        # self.master.rowconfigure(0, weight=1)
-        #
-        # self.frame.columnconfigure(1, weight=1)
-        # self.frame.rowconfigure(1, weight=1)
+        self.lbl_name.grid(column=1, row=0, columnspan=3, sticky='n')
+        self.btn_SIR.grid(column=1, row=1, sticky='s')
 
-    def close_window(self):
-        self.master.destroy()
+        self.master.columnconfigure(0, weight=1)
+        self.master.rowconfigure(0, weight=1)
+
+        self.frame.columnconfigure(1, weight=1)
+        self.frame.rowconfigure(1, weight=1)
+
+    def close(self):
+        pass
 
 
 root = tk.Tk()
