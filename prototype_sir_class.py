de86b1875b67c8cd9c68ff4afde9645828ebab2b
zjnmarsh <zjnmarsh@gmail.com>
2020-09-18 14:34:39 +0100

    Updated docstrings, created my own differential equation solver

--- prototype_sir_class.py
+++ prototype_sir_class.py
@@ -6,40 +6,67 @@ import matplotlib.pyplot as plt
 class SIR_model:
 
     def __init__(self):
+        # self.y0 = (0,0,0)
         pass
 
     def SIR_model(self, s0, i0, r0, beta, gamma, t, f, *args, **kwargs):
         """
-        :param s: number of susceptible people
-        :param i: number of infected people
-        :param r: number of recovered people
-        :param b: transmission rate of an individual
-        :param g: recovery rate of an individual
+        :param s0: number of susceptible people
+        :param i0: number of infected people
+        :param r0: number of recovered people
+        :param beta: transmission rate of an individual
+        :param gamma: recovery rate of an individual
         :param t: how long the simulation should run for
+        :param f: how many graphs should be calculated simultaneously
         """
 
         plt.figure(f)
-        t = np.linspace(0, t,
-                        t * 10)  # creates a time array at the points where the differential equations will be calculated, (start, end, number of timepoints)
+        # t = np.linspace(0, t,
+        #                 t * 10)  # creates a time array at the points where the differential equations will be calculated, (start, end, number of timepoints)
         N = s0 + i0 + r0  # total population
-        y0 = (s0, i0, r0)  # initial conditions
+        self.y0 = (s0, i0, r0)  # initial conditions
 
-        def eqns(y0, t, beta, gamma):
-            S, I, R = y0
-            dsdt = -(beta * S * I) / N  # rate of change of susceptible individuals
+        # print(t)
+        timearray = list(range(1, int(t))) # creates time array
+
+        # def eqns(y0, t, beta, gamma):
+        def eqns(param):
+            # e = 0.75
+            e = 0
+            S, I, R = param
+            dsdt = (-(beta * S * I) / N) + (e * R)  # rate of change of susceptible individuals
             didt = ((beta * S * I) / N) - gamma * I  # rate of change of infected individuals
-            drdt = gamma * I  # rate of change of recovered individuals
+            drdt = (gamma * I) - (e * R)  # rate of change of recovered individuals
             return dsdt, didt, drdt
 
-        result = odeint(eqns, y0, t, args=(beta, gamma))
-        print(result)
-        solution = np.array(result)
-        # print(solution)
-        plt.figure(figsize=[6, 4])
-        plt.plot(t, solution[:, 0], label="S(t)")
-        plt.plot(t, solution[:, 1], label="I(t)")
-        plt.plot(t, solution[:, 2], label="R(t)")
-        # plt.show()
+        def solver():
+            param = (s0, i0, r0)
+            solver_result = [[], [], []]
+            for time in timearray:
+                eqns_results = eqns(param)
+                x, y, z = (param[0] + eqns_results[0]), (param[1] + eqns_results[1]), (param[2] + eqns_results[2])
+                solver_result[0].append(x)
+                solver_result[1].append(y)
+                solver_result[2].append(z)
+                param = (x, y, z)
+            return solver_result
+
+        # result = odeint(eqns, y0, t, args=(beta, gamma))
+        solver_result = solver()
+        print(solver_result)
+
+        plt.plot(timearray, solver_result[0], label="S(t)")
+        plt.plot(timearray, solver_result[1], label="I(t)")
+        plt.plot(timearray, solver_result[2], label="R(t)")
+
+
+        # solution = np.array(result)
+        # # print(solution)
+        # plt.figure(figsize=[6, 4])
+        # plt.plot(t, solution[:, 0], label="S(t)")
+        # plt.plot(t, solution[:, 1], label="I(t)")
+        # plt.plot(t, solution[:, 2], label="R(t)")
+        # # plt.show()
 
 
 # sir_model = SIR_model()
@@ -52,7 +79,7 @@ class QueueSimulation:
         self.n = n  # number of simulations to be run
         self.parameters = []
         for i in range(n):
-            self.parameters.append([s_list[i], i_list[i], r_list[i], b_list[i], g_list[i], t, i+1])
+            self.parameters.append([s_list[i], i_list[i], r_list[i], b_list[i], g_list[i], t, i + 1])
         print(self.parameters)
 
     def run_simulation(self):
@@ -62,7 +89,8 @@ class QueueSimulation:
         plt.show()
 
 
+# queue = QueueSimulation(2, [999, 599], [1, 3], [0, 0], [0.2, 0.4], [0.1,0.1], 100)
+# queue = QueueSimulation(1, [999], [1], [0], [0.4], [0.1], 200)
+queue = QueueSimulation(1, [1], [0.01], [0], [0.4], [0.1], 100)
 
-
-queue = QueueSimulation(2, [999, 599], [1, 3], [0, 0], [0.2, 0.4], [0.1,0.1], 100)
 queue.run_simulation()
