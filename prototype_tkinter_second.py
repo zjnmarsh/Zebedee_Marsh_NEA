5a7848d1c87958fe63f7d3e577bda2f220fb5cf8
zjnmarsh <zjnmarsh@gmail.com>
2020-09-23 19:38:00 +0100

    Created input parameters window

--- prototype_tkinter_second.py
+++ prototype_tkinter_second.py
@@ -35,6 +35,7 @@ class Main_Window:
         new_window = SIR_Window(root2)
         root2.mainloop()
 
+
 class SIR_Window:
 
     def __init__(self, master):
@@ -42,12 +43,14 @@ class SIR_Window:
         self.frame = ttk.Frame(master, padding=5)
 
         self.lbl_name = ttk.Label(self.frame, text='This is the SIR model')
-        self.btn_SIR = ttk.Button(self.frame, text='Close', command=self.close)
+        self.btn_close = ttk.Button(self.frame, text='Close', command=self.close)
+        self.btn_input_param = ttk.Button(self.frame, text='Enter Parameters', command=self.input)
 
         self.frame.grid(row=0, column=0, sticky='nsew')
 
         self.lbl_name.grid(column=1, row=0, columnspan=3, sticky='n')
-        self.btn_SIR.grid(column=1, row=1, sticky='s')
+        self.btn_close.grid(column=1, row=2, sticky='s')
+        self.btn_input_param.grid(column=1, row=1)
 
         self.master.columnconfigure(0, weight=1)
         self.master.rowconfigure(0, weight=1)
@@ -58,11 +61,78 @@ class SIR_Window:
     def close(self):
         self.master.destroy()
 
+    def input(self):
+        root3 = tk.Tk()
+        root3.title('Input Parameters')
+        # root3.geometry('400x400')
+
+        input_window = SIR_Param(root3)
+        root3.mainloop()
+
+
+class SIR_Param:
+
+    def __init__(self, master):
+        self.master = master
+        self.frame = ttk.Frame(master, padding=5)
+        self.frame.grid(row=0, column=0, sticky='nsew')
+
+        # label variables
+        self.lbl_name = ttk.Label(self.frame, text='Enter parameters for SIR model')
+        self.s_name = ttk.Label(self.frame, text='Susceptible')
+        self.i_name = ttk.Label(self.frame, text='Infected')
+        self.r_name = ttk.Label(self.frame, text='Recovered')
+        self.tr_name = ttk.Label(self.frame, text='Transmission rate')
+        self.re_name = ttk.Label(self.frame, text='Recovery rate')
+        self.btn_enter = ttk.Button(self.frame, text='Enter', command=self.enter_param)
+
+        # button variables
+        self.e_s = ttk.Entry(self.frame)
+        self.e_i = ttk.Entry(self.frame)
+        self.e_r = ttk.Entry(self.frame)
+        self.e_tr = ttk.Entry(self.frame)
+        self.e_re = ttk.Entry(self.frame)
+
+
+        # gridding label variables
+        self.lbl_name.grid(column=0, row=0, columnspan=2, sticky='n')
+
+        self.s_name.grid(column=0, row=1, sticky='w')
+        self.i_name.grid(column=0, row=2, sticky='w')
+        self.r_name.grid(column=0, row=3, sticky='w')
+        self.tr_name.grid(column=0, row=4, sticky='w')
+        self.re_name.grid(column=0, row=5, sticky='w')
+
+        self.e_s.grid(column=1, row=1, sticky='w')
+        self.e_i.grid(column=1, row=2, sticky='w')
+        self.e_r.grid(column=1, row=3, sticky='w')
+        self.e_tr.grid(column=1, row=4, sticky='w')
+        self.e_re.grid(column=1, row=5, sticky='w')
+
+        self.btn_enter.grid(column=0, row=6, columnspan=2, sticky='s')
+
+        self.master.columnconfigure(0, weight=1)
+        self.master.rowconfigure(0, weight=1)
+
+        # weight elements for resizing
+        self.frame.columnconfigure(0, weight=1)
+        self.frame.rowconfigure(0, weight=1)
+        self.frame.rowconfigure(1, weight=1)
+        self.frame.rowconfigure(2, weight=1)
+        self.frame.rowconfigure(3, weight=1)
+        self.frame.rowconfigure(4, weight=1)
+        self.frame.rowconfigure(5, weight=1)
+        self.frame.rowconfigure(6, weight=1)
+
+    def enter_param(self):
+        self.master.destroy()
+
 
 root = tk.Tk()
 root.title('Main Window')
-root.geometry('600x400')
-
+# root.geometry('600x400')
+# root.geometry('400x400')
 window = Main_Window(root)
+# window = SIR_Param(root)
 
 root.mainloop()
