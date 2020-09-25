11b82d9c0a435d05c145a1ddf56818d5ac4e8a25
zjnmarsh <zjnmarsh@gmail.com>
2020-09-25 15:10:00 +0100

    Completed GUI for inputting parameters for a single graph

--- my_tkinter_file.py
+++ my_tkinter_file.py
@@ -1,5 +1,10 @@
 import tkinter as tk
 from tkinter import ttk
+from scipy.integrate import odeint
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import random
 
 
 class Main_Window:
@@ -28,29 +33,33 @@ class Main_Window:
         # self.new_window = tk.Toplevel(self.master)
         # self.app = SIR_Window(self.new_window)
 
+        self.master.destroy()
         root2 = tk.Tk()
         root2.title('SIR Model')
-        root2.geometry('1400x900')
-
+        # root2.geometry('1400x900')
         new_window = SIR_Window(root2)
         root2.mainloop()
 
 
 class SIR_Window:
+    """Class asking user to enter how many graphs they want"""
 
     def __init__(self, master):
         self.master = master
         self.frame = ttk.Frame(master, padding=5)
 
         self.lbl_name = ttk.Label(self.frame, text='This is the SIR model')
-        self.btn_close = ttk.Button(self.frame, text='Close', command=self.close)
+        self.num_sim = ttk.Entry(self.frame)
+        self.num_sim.insert(0, 1)
         self.btn_input_param = ttk.Button(self.frame, text='Enter Parameters', command=self.input)
+        self.btn_close = ttk.Button(self.frame, text='Close', command=self.close)
 
         self.frame.grid(row=0, column=0, sticky='nsew')
 
-        self.lbl_name.grid(column=1, row=0, columnspan=3, sticky='n')
-        self.btn_close.grid(column=1, row=2, sticky='s')
+        self.lbl_name.grid(column=0, row=0, columnspan=2, sticky='n')
+        self.num_sim.grid(column=0, row=1)
         self.btn_input_param.grid(column=1, row=1)
+        self.btn_close.grid(column=0, row=2, sticky='s')
 
         self.master.columnconfigure(0, weight=1)
         self.master.rowconfigure(0, weight=1)
@@ -62,17 +71,22 @@ class SIR_Window:
         self.master.destroy()
 
     def input(self):
+        self.number_of_simulations = int(self.num_sim.get())
+        # print(self.number_of_simulations)
+
         root3 = tk.Tk()
         root3.title('Input Parameters')
-        # root3.geometry('400x400')
-
-        input_window = SIR_Param(root3)
+        input_window = SIR_Param(root3, self.number_of_simulations)
         root3.mainloop()
 
 
-class SIR_Param:
 
-    def __init__(self, master):
+
+class SIR_Param:
+    """Class for entering SIR parameters"""
+
+    def __init__(self, master, num_sim):
+        self.number_of_simulations = num_sim
         self.master = master
         self.frame = ttk.Frame(master, padding=5)
         self.frame.grid(row=0, column=0, sticky='nsew')
@@ -96,7 +110,6 @@ class SIR_Param:
 
         # gridding label variables
         self.lbl_name.grid(column=0, row=0, columnspan=2, sticky='n')
-
         self.s_name.grid(column=0, row=1, sticky='w')
         self.i_name.grid(column=0, row=2, sticky='w')
         self.r_name.grid(column=0, row=3, sticky='w')
@@ -125,7 +138,16 @@ class SIR_Param:
         self.frame.rowconfigure(6, weight=1)
 
     def enter_param(self):
-        self.master.destroy()
+        # self.master.destroy()
+        param_list = [[],[],[],[],[]]
+        # for i in range(self.number_of_simulations):
+        param_list[0].append(float(self.e_s.get()))
+        param_list[1].append(float(self.e_i.get()))
+        param_list[2].append(float(self.e_r.get()))
+        param_list[3].append(float(self.e_tr.get()))
+        param_list[4].append(float(self.e_re.get()))
+        # param_list.append()
+        # print(param_list)
 
 
 root = tk.Tk()
