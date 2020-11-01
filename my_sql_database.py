2e71b90261e9fadc8196f8a9a1a6750fdaf795f6
zjnmarsh <zjnmarsh@gmail.com>
2020-11-01 18:00:02 +0000

    Created enter_username function

--- my_sql_database.py
+++ my_sql_database.py
@@ -7,7 +7,7 @@ def initial_setup():
     c = conn.cursor()
     c.execute("DROP TABLE users")
     c.execute("DROP TABLE ca_param")
-    c.execute("CREATE TABLE users (username text, see_all integer)")
+    c.execute("CREATE TABLE users (username text PRIMARY KEY, see_all integer)")
     c.execute("""CREATE TABLE ca_param (
                 no_cells integer,
                 generations integer,
@@ -20,6 +20,7 @@ def initial_setup():
                 use_immunity integer,
                 days_of_immunity integer    
                 )""")
+    conn.commit()
 
     # if not os.path.isfile('my_database.db'):
     #     # print("Creating new table")
@@ -31,14 +32,24 @@ def initial_setup():
     #
     #     new_table()
 
-# def enter_username(username):
+
+def enter_username(in_user):
+    conn = sqlite3.connect('my_database.db')
+    c = conn.cursor()
+    # c.execute("INSERT INTO users VALUES (:username, :see_all) ON DUPLICATE KEY UPDATE id=id",
+    #           {'username': in_user, 'see_all': 0})
+    c.execute("INSERT OR IGNORE INTO users VALUES (:username, :see_all)",
+              {'username': in_user, 'see_all': 0})  # don't add if duplicate username
+    conn.commit()
 
 
+# initial_setup()
+
+# enter_username("chase")
 
 
-
-
-
-
-
-initial_setup()
+# conn = sqlite3.connect('my_database.db')
+# c = conn.cursor()
+#
+# c.execute("SELECT * FROM users")
+# print(c.fetchall())
